493020bc38d42a9a90cb205dc564c6d34d7ea58a
AntonyDamico <antonydamico29@gmail.com>
2019-03-01 19:49:05 -0400

    moving the routes to app dir

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ require 'core/bootstrap.php';
 use Core\{Router, Request};
 
 try {
-    Router::load(require 'routes.php')
+    Router::load(require 'app\routes.php')
         ->direct(Request::uri());
 } catch (Exception $exception) {
     echo $exception->getMessage();
