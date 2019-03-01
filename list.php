66f49b3340a8f1e530263662659934484be80504
AntonyDamico <antonydamico29@gmail.com>
2019-03-01 17:35:00 -0400

    moving contact list to its view

--- list.php
+++ list.php
@@ -5,14 +5,15 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Index</title>
+    <title>Document</title>
 </head>
 <body>
-
-<h1>Welcome to Contacts</h1>
+<h1>Contact List</h1>
 
 <ul>
-    <li><a href="">List</a></li>
+    <?php foreach ($contacts as $contact): ?>
+        <li><?= $contact->first_name . ' ' . $contact->last_name ?></li>
+    <?php endforeach ?>
 </ul>
 
 </body>
