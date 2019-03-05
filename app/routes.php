28787fc2420ca1d4988c9276b06598984ebb67ea
AntonyDamico <antonydamico29@gmail.com>
2019-03-04 22:39:59 -0400

    creating route

--- app/routes.php
+++ app/routes.php
@@ -3,7 +3,8 @@
 return [
     'GET' => [
         '' => 'PagesController@home',
-        'contacts' => 'ContactsController@list'
+        'contacts' => 'ContactsController@list',
+        'contacts/read' => 'ContactsController@read'
     ],
 
     'POST' => [
