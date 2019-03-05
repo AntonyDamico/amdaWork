3b22aafd630ab4ab407ba9bd8560d4aeea4357d7
AntonyDamico <antonydamico29@gmail.com>
2019-03-04 23:08:21 -0400

    showing a contact by id

--- app/views/list.view.php
+++ app/views/list.view.php
@@ -1,11 +1,15 @@
 <?php include 'templates/header.view.php' ?>
 
-<h1>Contact List</h1>
+    <h1>Contact List</h1>
 
-<ul>
-    <?php foreach ($contacts as $contact): ?>
-        <li><?= $contact->first_name . ' ' . $contact->last_name ?></li>
-    <?php endforeach ?>
-</ul>
+    <ul>
+        <?php foreach ($contacts as $contact): ?>
+            <li>
+                <a href="/contacts/read?id=<?= $contact->id ?>">
+                    <?= $contact->first_name . ' ' . $contact->last_name ?>
+                </a>
+            </li>
+        <?php endforeach ?>
+    </ul>
 
 <?php include "templates/footer.view.php";
