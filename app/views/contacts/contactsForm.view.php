af3927855d40910071e7484420dff34d0d245f9c
AntonyDamico <antonydamico29@gmail.com>
2019-03-04 22:34:29 -0400

    updating views

--- app/views/contacts/contactsForm.view.php
+++ app/views/contacts/contactsForm.view.php
@@ -1,11 +1,13 @@
 <?php //require '../templates/header.view.php'?>
 
-<form action="" method="POST">
+<form action="/contacts/add" method="POST">
     <label for="firstName">First Name:</label>
     <input type="text" name="firstName" id="firstName">
 
     <label for="lastName">Last Name:</label>
     <input type="text" name="lastName" id="lastName">
+
+    <input type="submit" value="Submit">
 </form>
 
 <?php //include '..\templates\footer.view.php' ?>
--- app/views/templates/nav.php
+++ app/views/templates/nav.php
@@ -2,6 +2,6 @@
     <ul>
         <li><a href="/">Home</a></li>
         <li><a href="contacts">Contacts</a></li>
-        <li><a href="contacts/add">Add Contact</a></li>
+        <li><a href="/contacts/add">Add Contact</a></li>
     </ul>
 </nav>
